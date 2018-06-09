1b329551325b652340e3df87e2c6c700a58c9b78
codey4k <codey4k@gmail.com>
2018-06-09 23:05:18 +0000

    update 09/06/2018

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+const menu = document.getElementById('iconmenu'),
+      wrapper = document.getElementsByClassName('wrapper')
+
+menu.addEventListener('click',()=>{
+    wrapper.style.marginLeft = 0
+})
